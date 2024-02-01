8283bfb4a77df119db9e314c94b8d176bad7d501
dell <m>
2024-02-01 22:20:47 +0600

    Initial commit

---
+++ src/test/java/com/neobis/neoCafe/NeoCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neobis.neoCafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
