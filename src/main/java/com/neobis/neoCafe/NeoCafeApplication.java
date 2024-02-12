4e571fb8cab3673b2e1fa8b9708815d8717895fa
dell <m>
2024-02-12 14:59:22 +0600

    Configure server url for swagger

--- src/main/java/com/neobis/neoCafe/NeoCafeApplication.java
+++ src/main/java/com/neobis/neoCafe/NeoCafeApplication.java
@@ -1,9 +1,17 @@
 package com.neobis.neoCafe;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.neobis.neoCafe")
+@ComponentScan(basePackages = "com.neobis.neoCafe")
+@OpenAPIDefinition(servers = {
+		@Server(url = "/", description = "Default Server URL")})
 public class NeoCafeApplication {
 
 	public static void main(String[] args) {
