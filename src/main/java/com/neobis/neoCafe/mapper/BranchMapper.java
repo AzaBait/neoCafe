f89960a093e996d3c8f1851a53a1f50f1049e7b0
dell <m>
2024-03-08 10:08:19 +0600

    Try to fix mapping

--- src/main/java/com/neobis/neoCafe/mapper/BranchMapper.java
+++ src/main/java/com/neobis/neoCafe/mapper/BranchMapper.java
@@ -1,16 +1,13 @@
 package com.neobis.neoCafe.mapper;
 
 import com.neobis.neoCafe.dto.BranchDto;
-import com.neobis.neoCafe.dto.WorkScheduleDto;
 import com.neobis.neoCafe.entity.Branch;
-import com.neobis.neoCafe.entity.WorkSchedule;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring", uses = {WorkScheduleMapper.class})
 public interface BranchMapper {
